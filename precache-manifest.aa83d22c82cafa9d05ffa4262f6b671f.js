fa82698e84a949e9e77e316eebcb403bcfc2db43
leye195 <leye195@naver.com>
2020-06-08 16:59:28 +0900

    Updates

---
+++ precache-manifest.aa83d22c82cafa9d05ffa4262f6b671f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aed7d13f0878104476af96c104fac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0b2d0afda51e52ecd3",
+    "url": "/static/js/2.c21b4763.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c21b4763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9a30b403d1611d0ec0",
+    "url": "/static/js/main.e8533d31.chunk.js"
+  },
+  {
+    "revision": "e973c8235c56a4efcbda",
+    "url": "/static/js/runtime-main.65876d70.js"
+  }
+]);
